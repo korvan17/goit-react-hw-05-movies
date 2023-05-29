1272aaf19b62256c678cd12e618ed7508a991629
Ivan Kornienko <ivan@homeds.pro>
2023-05-29 23:02:17 +0300

    add route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,31 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Movies from '../Movies/Movies';
+import styled from 'styled-components';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework 888 template
+    <div>
+      <nav>
+        <li>
+          <StyledLink to="/">Home</StyledLink>
+        </li>
+        <li>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </li>
+      </nav>
+      <Routes>
+        <Route path="/" element={<>Домашняя страница</>} />
+        <Route path="/movies" element={<Movies />} />
+        <Route />
+      </Routes>
     </div>
   );
 }
